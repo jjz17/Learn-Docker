d0dc57e4e69e604272e57cde06af824765a40521
Jason Zhang <jasonjzhang17@gmail.com>
2022-05-09 16:05:55 -0400

    Successfully ran Docker container with user input

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ def main():
 
     n_movies = len(titles)
 
-    # while(True):
-    idx = random.randrange(0, n_movies)
+    while(True):
+        idx = random.randrange(0, n_movies)
         
-    print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
+        print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
 
-        # user_input = input('Do you want another movie (y/[n])? ')
-        # if user_input != 'y':
-        #     break
+        user_input = input('Do you want another movie (y/[n])? ')
+        if user_input != 'y':
+            break
     
 
 if __name__ == '__main__':
