3f7e31795539f14904f3f08c20e0118b4ff154a3
Jason Zhang <jasonjzhang17@gmail.com>
2022-05-11 11:23:52 -0400

    Learning pytest

---
+++ flask_redis/tests/unit/test_practice.py
@@ -0,0 +1,11 @@
+
+def test_addition():
+    '''
+    GIVEN two numbers
+    WHEN they are added together
+    THEN check that the result is equivalent to the expected sum
+    '''
+
+    num1 = 3
+    num2 = 4
+    assert num1+num2 == 7
