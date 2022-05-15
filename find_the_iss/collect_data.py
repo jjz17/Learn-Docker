89c3abbddde95f507421c5f7ac27230333bf5a8a
Jason Zhang <jasonjzhang17@gmail.com>
2022-05-15 15:45:01 -0400

    Automated data collection with cronjob

--- find_the_iss/collect_data.py
+++ find_the_iss/collect_data.py
@@ -4,8 +4,8 @@ import time
 
 file = open('json_data.json')
 data = json.load(file)
-print(type(data))
-print(data)
+# print(type(data))
+# print(data)
 
 # API link
 url = 'http://api.open-notify.org/iss-now.json'
@@ -16,12 +16,12 @@ for _ in range(5):
     iss_loc = iss_data['iss_position']
     iss_latlon = {'lat': float(iss_loc['latitude']), 'lon': float(iss_loc['longitude'])}
     new_data[int(time.time())] = (iss_latlon)
-    print('running...')
+    # print('running...')
     time.sleep(5)
 
 # Append new data to existing data
 data.update(new_data)
-print(data)
+# print(data)
 # json_string = json.dumps(data)
 
 with open('json_data.json', 'w') as outfile:
