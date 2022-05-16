17d5a93ca6f58e68d42c4cd9f288b4e52066bfc3
Jason Zhang <jasonjzhang17@gmail.com>
2022-05-15 20:44:28 -0400

    Updated data collection methodology to increase data size

--- find_the_iss/collect_data.py
+++ find_the_iss/collect_data.py
@@ -11,13 +11,13 @@ data = json.load(file)
 url = 'http://api.open-notify.org/iss-now.json'
 new_data = {}
 
-for _ in range(5):
+for _ in range(50):
     iss_data = json.loads(requests.get(url).text)
     iss_loc = iss_data['iss_position']
     iss_latlon = {'lat': float(iss_loc['latitude']), 'lon': float(iss_loc['longitude'])}
     new_data[int(time.time())] = (iss_latlon)
     # print('running...')
-    time.sleep(5)
+    time.sleep(30)
 
 # Append new data to existing data
 data.update(new_data)
